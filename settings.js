33970daea888582effd9dfa2aaefe69d797ebeae
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-25 20:28:24 -0300

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.etiqueta = 'Demitra';
 global.moneda = 'Stamps';
 global.currencySymbol = 'Stamps';
 
-global.welcom1 = '¡Bienvenido a mi paraíso rosado! 💗\n✨ Soy Zero Two ✨\n🌸 Edita este mensaje con setwelcome 🌸';
+global.welcom1 = '🎩 Bienvenido al grupo. Demitra observa… pero no pregunta demasiado.\n🖤 Respeta las reglas y todo irá bien.\n💻 Edita este mensaje con setwelcome';
 global.welcom2 = '💔 ¡Hasta la próxima, Darling! Gracias por estar aquí~\n🌸 ¡Espero verte pronto de nuevo! 🌸\n💗 Edita este mensaje con setbye 💗';
 
 global.banner = 'https://wallpapers.com/images/hd/zero-two-pictures-1j4mw86y6ncyfvj2.jpg';
