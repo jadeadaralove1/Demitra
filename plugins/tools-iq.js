20ba151d9dcfce43551eb692b7faf45091959a59
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 01:47:05 -0300

    Crear tools-iq.js

---
+++ plugins/tools-iq.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn }) => {
+
+let iq = Math.floor(Math.random() * 200)
+
+let msg = `🧠 *Calculando IQ...*\n\nTu IQ es: *${iq}*`
+
+conn.reply(m.chat, msg, m)
+
+}
+
+handler.help = ['iq']
+handler.tags = ['fun']
+handler.command = ['iq']
+
+export default handler
