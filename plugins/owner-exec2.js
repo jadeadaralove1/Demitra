e704a1a563e8f541e2d2fabc437bf9c6ce2e1263
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 01:37:41 -0300

    Crear owner-exec2.js

---
+++ plugins/owner-exec2.js
@@ -0,0 +1,30 @@
+import cp, { exec as _exec } from 'child_process'
+import { promisify } from 'util'
+
+const exec = promisify(_exec).bind(cp)
+
+let handler = async (m, { conn, args }) => {
+    const text = args.join(' ')
+    if (!text) return m.reply('💗 Darling, ingresa un comando~')
+
+    await m.reply('💗 *Ejecutando, darling~*')
+
+    let o
+    try {
+        o = await exec(text.trimEnd())
+    } catch (e) {
+        o = e
+    } finally {
+        const { stdout, stderr } = o
+        if (stdout?.trim()) await m.reply(stdout.trim())
+        if (stderr?.trim()) await m.reply(stderr.trim())
+    }
+}
+
+handler.help = ['$']
+handler.tags = ['owner']
+handler.customPrefix = ['$']
+handler.command = new RegExp
+handler.rowner = true
+
+export default handler
