2b63a9010b4a941d4b3a920180865fb13e864d3b
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 10:42:06 -0300

    Actualizar grupos-invocar.js

--- plugins/grupos-invocar.js
+++ plugins/grupos-invocar.js
@@ -5,15 +5,15 @@ const handler = async (m, { conn, args, isAdmin, isOwner }) => {
         const group = await conn.groupMetadata(m.chat)
         const participants = group.participants.map(p => p.jid || p.id.split(':')[0] + '@s.whatsapp.net')
 
-        const anuncio = args.join(' ') || '¡Todos atentos que mi Darling quiere decir algo! 💗'
+        const anuncio = args.join(' ') || '¡Todos atentos!'
 
         const mentions = participants.map(p => `@${p.split('@')[0]}`).join(' ')
 
         const caption =
-            `💞 *¡MI DARLING HA INVOCADO A TODO EL GRUPO!* 🌸\n\n` +
-            `💗 *Anuncio:* ${anuncio}\n\n` +
+            `*¡HAS INVOCADO A TODO EL GRUPO!*\n\n` +
+            `*Anuncio:* ${anuncio}\n\n` +
             `${mentions}\n\n` +
-            `¡Respondan rapidito no me dejen sola esperando~ 💕`
+            `¡Respondan rapido no me dejen sola esperando!`
 
         await conn.sendMessage(m.chat, {
             image: { url: 'https://causas-files.vercel.app/fl/xxbz.jpg' },
@@ -21,12 +21,12 @@ const handler = async (m, { conn, args, isAdmin, isOwner }) => {
             mentions: participants
         }, { quoted: m })
 
-        await m.react('💗')
+        await m.react('🪻')
 
     } catch (e) {
         console.error('❌ INVOCAR ERROR:', e)
-        await m.react('💔')
-        m.reply('💔 Uy darling... la invocación falló esta vez~\nInténtalo de nuevo no me dejes sola 🌸')
+        await m.react('😞')
+        m.reply('😞Uy... la invocación falló esta vez\nInténtalo de nuevo 🦭')
     }
 }
 
