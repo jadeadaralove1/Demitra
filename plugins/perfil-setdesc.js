b1e8803ce424305ed6599d905cd1ab0eb05497d2
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-27 18:03:06 -0300

    Crear perfil-setdesc.js

---
+++ plugins/perfil-setdesc.js
@@ -0,0 +1,37 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+  // ✅ FIX DB
+  global.db = global.db || {}
+  global.db.data = global.db.data || {}
+  global.db.data.users = global.db.data.users || {}
+
+  const userId = m.sender
+  // Inicializar usuario si no existe
+  if (!global.db.data.users[userId]) {
+    global.db.data.users[userId] = {
+      name: 'Sin nombre',
+      genre: 'Oculto',
+      description: '',
+      pasatiempo: 'No definido',
+      exp: 0,
+      level: 0,
+      coins: 0,
+      bank: 0,
+      marry: null
+    }
+  }
+
+  const user = global.db.data.users[userId]
+
+  const input = args.join(' ')
+  if (!input) return m.reply(`ᘛ  ࣭   🪼⃝    ࣪   ࣭Debes especificar una descripción válida para tu perfil.\n\n> Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`)
+
+  user.description = input
+  return m.reply(`   ࣪   🐢  ࣭  Se ha establecido tu descripción, puedes revisarla con ${usedPrefix}profile ૮₍˶• ˔ ก₎ა`)
+}
+
+handler.help = ['setdescription', 'setdesc']
+handler.tags = ['rpg']
+handler.command = ['setdescription', 'setdesc']
+
+export default handler
