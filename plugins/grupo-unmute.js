802eb811010b15a0cbbaa8728a4a35df60815870
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-29 12:30:58 -0300

    Actualizar grupo-unmute.js

--- plugins/grupo-unmute.js
+++ plugins/grupo-unmute.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, prefix, isAdmin, isBotAdmin }) => {
     await database.save()
 
     await conn.sendMessage(m.chat, {
-        text: ` @${who.split('@')[0]} ha sido desmuteado.`,
+        text: `𐄹 ۪ ׁ 🔊ᩚ̼ 𖹭̫ ▎@${who.split('@')[0]} ha sido desmuteado.`,
         mentions: [who]
     })
 }
